108fa078d19e650d207f878d1b2d31ca9ea3cc07
chrisk4040 <526ckozeniesky@frhsd.com>
2025-03-26 09:31:48 -0400

    need to fix my code dn what happened

--- script.js
+++ script.js
@@ -9,29 +9,25 @@ const wordList = [
 
 let selectWord = '';
 let displayedWord = '';
-let wrongGuess = 0;
-let guessedWord = []
+let wrongGuesses = 0;
+let guessLetters = []
 const maxMistakes = 6 
 
 function startGame(level){
     selectWord = getRandomWord(level);
 
-//update diff display div
- updateDifficultyDisplay(level)
+    //update diff display div
+    updateDifficultyDisplay(level)
 
- //create place holder for selected word 
- displayedWord = '_'.repeat(selectWord.length);
- document.getElementById('display-word').textContent = displayedWord.split('').join(' ');
-    //hide diff sleect and show game area
+    //create place holder for selected word 
+    displayedWord = '_'.repeat(selectWord.length);
+    document.getElementById('display-word').textContent = displayedWord.split('').join(' ');
 
-    //hide diff
+    //hide diff select and show game area
     document.getElementById('difficulty-selection').classList.add('d-none')
-
-    //remove d-npne from difficulty box and game area
     document.getElementById('game').classList.remove('d-none')
     document.getElementById('difficulty-box').classList.remove('d-none')
     document.getElementById('difficulty-selection').classList.add('d-none')
-    //add d-block to #difficultybox
     document.getElementById('difficulty-selection').classList.add('d-none')
     document.getElementById('difficulty-box').classList.add('d-block')
 }
@@ -41,12 +37,9 @@ function getRandomWord(level){
     if (level === 'easy') return word.length <= 4;
     if (level === 'medium') return word.length >= 5 && word.length <= 7;
     if (level === 'hard') return word.length >= 8;
-
    }) 
 
-   
    return filWords[Math.floor(Math.random() * filWords.length)];
-
 }
 
 function updateDifficultyDisplay(level){
@@ -60,3 +53,76 @@ function updateDifficultyDisplay(level){
     //apply the appropriate css style for chosen Difficulty
     diffbox.classList.add(level);
 }
+
+function guessLetter(){
+    let inputField = document.getElementById('letter-input');
+    let guessLetter = inputField.value.toLowerCase();
+
+    //check if value is between a-z
+    if (!guessLetter.match(/^[a-z]$/) ){
+         alert('Guess Between a-z')
+         inputField.value = '';
+         return
+    }
+
+    if (guessLetters.includes(guessLetter)){
+        alert('You already guessed this letter');
+        inputField.value = '';
+        return
+    } else {
+        guessLetters.push(guessLetter);
+        inputField.value = '';
+    }
+
+    if(selectWord.includes(guessLetter)){
+        correctGuess(guessLetter)
+    } else {
+        wrongGuess(guessLetter)
+    }
+
+    inputField.value = '';
+    inputField.focus()
+}
+
+function wrongGuess(guessedLetter){
+    // increment the number of wrong guesses 
+    wrongGuesses++;
+
+    // add guessed letter to the guessed Letters array
+    document.getElementById('wrong-letters').textContent += `${guessedLetter}`
+
+    document.getElementById('shamrock').src = `img/shamrock${6-wrongGuesses}.jpg` // remember to have photos with the same name and file type 
+
+    if(wrongGuesses === maxMistakes){
+        endGame(false)
+    } // check to see if the number of wrong guesses === max mistakes
+}
+
+
+function correctGuess(guessedLetter){
+    let newDisplayedWord = ''
+
+    for (let i = 0; i < selectWord.length; i++) {
+        if(selectWord[i] === guessedLetter){
+            newDisplayedWord += guessedLetter ;
+} else {
+    newDisplayedWord += displayedWord[i];
+}
+}
+
+displayedWord = newDisplayedWord;
+document.getElementById('display-word').textContent = displayedWord;
+.split('')
+.join(' ')
+}
+
+function endGame(won){
+    if (won === true){
+      setTimeout(() => alert('yeay you won'), 100)
+    }else {
+    }
+  }
+
+  function restartGame(){
+    location.reload()
+  }
